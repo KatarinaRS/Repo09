ff2902b5ab2c215908ec552d89f9337c7621bf49
KatarinaRS <katarina.roser@gmail.com>
2022-04-05 18:53:50 +0200

    Boolean values

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+//Only change code bellow this line
+function myTrueBooelan() {
+    return true;
+}
+console.log(myTrueBooelan());
+//Only change code above this line
+module.exports = myTrueBooelan
