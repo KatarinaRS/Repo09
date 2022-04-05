9bd997e63c973d0d3cfbb6ad62ef73e29788a95e
KatarinaRS <katarina.roser@gmail.com>
2022-04-05 22:14:56 +0200

    Switch statements

---
+++ ex15/src/index.js
@@ -0,0 +1,23 @@
+//Only change code below this line
+function switchCase(letter) {
+    var animals =" ";
+    switch(letter) {
+        case "a": animals = "antilope";
+        break;
+        case "b": animals = "bird";
+        break;
+        case "c": animals = "car";
+        break;
+        case "d": animals = "stuff";
+        break;
+        default: animals = "stuff";
+    }
+    return animals;
+}
+ console.log(switchCase("a"));
+ console.log(switchCase("b"));
+ console.log(switchCase("c"));
+ console.log(switchCase("d"));
+ console.log(switchCase(4));
+ //Only change code above this line 
+ module.exports = switchCase;
